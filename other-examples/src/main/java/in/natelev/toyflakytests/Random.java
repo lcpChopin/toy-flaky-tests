47e0eb195793111b9f2044d67835605c72409732
LI CHENGPENG (cl46824) <jingzhaolicheng@163.com>
2024-01-24 16:13:53 -0600

    update

--- other-examples/src/main/java/in/natelev/toyflakytests/Random.java
+++
@@ -1,9 +0,0 @@
-package in.natelev.toyflakytests;
-
-import java.util.concurrent.ThreadLocalRandom;
-
-public class Random {
-    public static boolean randomBoolean() {
-        return ThreadLocalRandom.current().nextBoolean();
-    }
-}
